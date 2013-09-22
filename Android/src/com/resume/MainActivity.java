72684a507d06acffbbe1ba95e552a15590984dd3
Adam Hart <pididiot1@gmail.com>
2013-09-21 22:56:43 -0500

    Pre JNI fucking

--- Android/src/com/resume/MainActivity.java
+++ Android/src/com/resume/MainActivity.java
@@ -1,19 +1,16 @@
 package com.resume;
 
 
-import android.os.Bundle;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.graphics.Typeface;
+import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
 
 public class MainActivity extends Activity {
--- Android/src/com/resume/MainFragment.java
+++ Android/src/com/resume/MainFragment.java
@@ -1,12 +1,29 @@
 package com.resume;
 
+import java.util.List;
+
+import android.app.Dialog;
 import android.app.Fragment;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+
+import com.parse.FindCallback;
+import com.parse.GetDataCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
 
 public class MainFragment extends Fragment{
+	
+	private Dialog progressDialog;
+	
 	public MainFragment() {
 		
 	}
@@ -16,6 +33,37 @@ public class MainFragment extends Fragment{
             Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 		getActivity().setTitle("Home Page");
+		
+		getResume();
+	    
+		
 		return rootView;
 	}
+	
+	private void getResume() {
+		MainFragment.this.progressDialog = ProgressDialog.show(getActivity(), "",
+				"Fetching Resumes...", true);
+		ParseQuery<ParseObject> query = ParseQuery.getQuery("Resume");
+		query.whereEqualTo("user", ParseUser.getCurrentUser());
+		query.findInBackground(new FindCallback<ParseObject>() {
+		    public void done(List<ParseObject> resumeList, ParseException e) {
+		        if (e == null) {
+		        	openResume(resumeList);
+		            Log.d("Debug", "Retrieved " + resumeList.size() + " resumes");
+		            MainFragment.this.progressDialog.dismiss();
+		        } else {
+		            Log.d("Debug", "Error: " + e.getMessage());
+		            MainFragment.this.progressDialog.dismiss();
+		        }
+		    }
+		});
+	}
+	private void openResume(List<ParseObject> resumeList) {
+		ParseFile applicantResume = (ParseFile)resumeList.get(0).get("applicantResumeFile");
+		WebView mWebView = (WebView) getActivity().findViewById(R.id.wvPdfView);
+	    mWebView.getSettings().setJavaScriptEnabled(true);
+//	    mWebView.getSettings().setPluginsEnabled(true);
+	    mWebView.loadUrl("https://docs.google.com/gview?embedded=true&url="+applicantResume.getUrl());
+	}
+	
 }
